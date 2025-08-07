b31ec6d83f18a70fcde9fc2879a68a4e399e57c5
Nikita Pedorich <pedorich.n@gmail.com>
2025-08-07 23:04:20 +0900

    Switch to nixfmt

--- flake.nix
+++ flake.nix
@@ -12,64 +12,77 @@
     };
   };
 
-  outputs = inputs@{ flake-parts, systems, self, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = import systems;
-    imports = [ inputs.treefmt-nix.flakeModule ];
+  outputs =
+    inputs@{
+      flake-parts,
+      systems,
+      self,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+      imports = [ inputs.treefmt-nix.flakeModule ];
 
-    perSystem = { system, pkgs, lib, ... }: {
-      devShells = {
-        default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            bashInteractive
-            git
-            just
-            python3
-            python3Packages.argcomplete
+      perSystem =
+        {
+          system,
+          pkgs,
+          lib,
+          ...
+        }:
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              nativeBuildInputs = with pkgs; [
+                bashInteractive
+                git
+                just
+                python3
+                python3Packages.argcomplete
 
-            esphome
-            platformio-core
-            platformio
-          ];
+                esphome
+                platformio-core
+                platformio
+              ];
 
-          shellHook = ''
-            export INSIDE_NIX_DEVELOP=true
-            export ESPHOME_NOGITIGNORE=true
+              shellHook = ''
+                export INSIDE_NIX_DEVELOP=true
+                export ESPHOME_NOGITIGNORE=true
 
-            python --version
-            platformio --version
-            echo "ESPHome $(esphome version)"
-          '';
-        };
-      };
-
-      treefmt = {
-        projectRootFile = ".root";
-        flakeCheck = true;
-
-        settings.formatter = {
-          black.options = [ "--line-length=120" ];
-        };
-
-        programs = {
-          # Nix
-          nixpkgs-fmt.enable = true;
-
-          # C++
-          clang-format.enable = true;
-
-          # Python
-          black.enable = true;
-          isort = {
-            enable = true;
-            profile = "black";
+                python --version
+                platformio --version
+                echo "ESPHome $(esphome version)"
+              '';
+            };
           };
 
-          # YAML, etc
-          prettier.enable = true;
-          just.enable = true;
-        };
-      };
-    };
-  };
-}
+          treefmt = {
+            projectRootFile = ".root";
+            flakeCheck = true;
 
+            settings.formatter = {
+              black.options = [ "--line-length=120" ];
+            };
+
+            programs = {
+              # Nix
+              nixfmt.enable = true;
+
+              # C++
+              clang-format.enable = true;
+
+              # Python
+              black.enable = true;
+              isort = {
+                enable = true;
+                profile = "black";
+              };
+
+              # YAML, etc
+              prettier.enable = true;
+              just.enable = true;
+            };
+          };
+        };
+    };
+}
