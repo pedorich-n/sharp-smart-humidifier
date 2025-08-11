96dbed8dcd1b3f0837f08b569d64b96ebc08c0c7
Nikita Pedorich <pedorich.n@gmail.com>
2025-08-11 13:10:02 +0900

    WIP. More setup

---
+++ ha_component/custom_components/sharp_smart_humidifier/__init__.py
@@ -0,0 +1,11 @@
+from homeassistant import core
+
+DOMAIN = "sharp_smart_humidifier"
+
+async def async_setup(hass: core.HomeAssistant, config: dict):
+    """Set up the Sharp HV-R75 Smart Humidifier component."""
+
+    # TODO: Implement
+
+    # Return boolean to indicate that initialization was successful.
+    return True
